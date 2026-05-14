4b132bcce43976889024d3842ac14dc6b8f0ab14
sukullcom <sukull.com@gmail.com>
2026-05-14 07:32:51 +0300

    new images added without background

--- app/(main)/(protected)/error.tsx
+++ app/(main)/(protected)/error.tsx
@@ -5,7 +5,7 @@ import { SegmentError } from "@/components/segment-error";
 export default function ProtectedError({
   error,
   reset,
-}: {
+}:{
   error: Error & { digest?: string };
   reset: () => void;
 }) {
